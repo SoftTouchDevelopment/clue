1bcc21ee10bc5fe85a7ae96a9f6ea97cd2612f23
YGao <ygao@signiant.com>
2017-11-14 22:14:34 -0500

    select players

---
+++ Clue/playersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  playersTableViewCell.swift
+//  Clue
+//
+//  Created by Yan Gao on 2017-11-14.
+//  Copyright © 2017 Micheal Montpetit. All rights reserved.
+//
+
+import UIKit
+
+class playersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
